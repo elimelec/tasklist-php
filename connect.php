2723fce34bf1d2d2ac8d326c1bf5c3f7ccb0da0e
MihaelaNicoleta <raumiha21@gmail.com>
2015-05-08 18:50:36 +0300

    connect

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+	$host = "localhost";
+	$password = "root";
+	$user = "root";
+	$db = "pufulist";
+
+
+	$mysqli = mysqli_connect($host, $user, $password, $db) or die(mysqli_connect_error());
+
+?>
