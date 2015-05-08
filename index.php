7516f45f1d96097edebe156ce159e9d62e5fa1b2
Eli <coloredflare@gmail.com>
2015-05-08 17:28:24 +0300

    Super basic log in

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PufuList</title>
+	</head>
+	<body>
+		<h1>PufuList</h1>
+		<form action="lists.php" method="GET">
+			<label for="username">Username: </label>
+			<input id="username" type="text" name="username">
+			<input type="submit" value="Login">
+		</form>
+	</body>
+</html>
---
+++ lists.php
@@ -0,0 +1,5 @@
+<?php
+	if (isset($_GET["username"])) {
+		echo "Welcome back, " . $_GET["username"] . "! You, awesome being.";
+	}
+?>
