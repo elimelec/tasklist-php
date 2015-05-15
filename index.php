98ecca8cba5b80948f1abd9cfb4f087165f1f1a1
MihaelaNicoleta <raumiha21@gmail.com>
2015-05-15 16:57:37 +0300

    login on index

--- index.php
+++ index.php
@@ -5,10 +5,24 @@
 	</head>
 	<body>
 		<h1>PufuList</h1>
+		<p> Log in </p>
+
 		<form action="lists.php" method="GET">
 			<label for="username">Username: </label>
 			<input id="username" type="text" name="username">
 			<input type="submit" value="Login">
-		</form>
+		</form>	
+
+		<p> Sign Up </p>
+		<form action="lists.php" method="GET">
+			<label for="username">Username: </label>
+			<input id="username" type="text" name="username"> <br>
+			<label for="username">Password: </label>
+			<input id="password" type="text" name="password"> <br>
+			<label for="username">Password: </label>
+			<input id="password" type="text" name="password"> <br>
+			<input type="submit" value="Sign Up">
+		</form>	
+
 	</body>
 </html>
