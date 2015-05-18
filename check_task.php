337f0c2b34ab042417a3a1962b8acf16f23adbfc
Eli <coloredflare@gmail.com>
2015-05-18 09:34:01 +0300

    redirect back after checking a task

--- check_task.php
+++ check_task.php
@@ -4,14 +4,16 @@
 
 	if (isset($_GET['task_id'])) {
 		$task_id = $_GET['task_id'];
-		$sql = 	"SELECT checked FROM tasks WHERE id = $task_id";
+		$sql = 	"SELECT checked, list_id FROM tasks WHERE id = $task_id";
 		
-		$is_checked = $mysqli->query($sql)->fetch_object()->checked;
-		var_dump($is_checked);
+		$result = $mysqli->query($sql)->fetch_object();
+		$is_checked = $result->checked;
+		$list_id = $result->list_id;
+
 		$is_checked = $is_checked == 1 ? 0 : 1;
 
 		$sql = "UPDATE tasks set checked = $is_checked WHERE id = $task_id";
 		$mysqli->query($sql);
-
-		echo "Checked/Unchecked task";
+		
+		header("Location: tasks.php?list_id=$list_id");
 	}
