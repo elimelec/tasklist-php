40d98e4b955652516e2b51b8e9e5e7ba2668fee3
Eli <coloredflare@gmail.com>
2015-05-18 09:37:26 +0300

    remove useless *thing*

--- lists.php
+++ lists.php
@@ -17,7 +17,7 @@
 		while($row = $results->fetch_assoc()) {
 			$id = $row["id"];
 			$name = $row["name"];
-			echo "$name <a href='tasks.php?user_id=$user_id&list_id=$id'>[show tasks]</a> <a href='edit_list.php?list_id=$id'>[edit]</a> <a href='delete_list.php?list_id=$id'>[delete]</a> <br>";
+			echo "$name <a href='tasks.php?list_id=$id'>[show tasks]</a> <a href='edit_list.php?list_id=$id'>[edit]</a> <a href='delete_list.php?list_id=$id'>[delete]</a> <br>";
 		}
 	}
 ?>
