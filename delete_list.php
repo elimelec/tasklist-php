ffb18c5e8b27f271437dd4087d574a1654702b8e
Eli <coloredflare@gmail.com>
2015-05-19 08:44:29 +0300

    redirect after deleting a list

--- delete_list.php
+++ delete_list.php
@@ -6,18 +6,16 @@
 	if(isset($_GET['list_id'])) {
 
 		$list_id = $_GET['list_id'];
-		$sql_select_list = "SELECT name FROM lists WHERE id='". $list_id."'";
-		$list_name = $mysqli->query($sql_select_list)->fetch_object()->name;
+		$sql_select_list = " SELECT user FROM lists JOIN users ON users.id = user_id WHERE lists.id = $list_id";
+		$username = $mysqli->query($sql_select_list)->fetch_object()->user;
 
 		$sql_delete_tasks = "DELETE FROM tasks WHERE list_id =" .$list_id;
 		$delete_tasks = $mysqli->query($sql_delete_tasks);
 
-
 		$sql_delete_list = "DELETE FROM lists WHERE id='". $list_id."'";
 		$result = $mysqli->query($sql_delete_list);
 
-		echo "List: $list_name was deleted";
-
+		header("Location: lists.php?username=$username");
 	}
 
 
