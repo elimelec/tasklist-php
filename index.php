fd9c0a804c10f6335eff7f0482a7e32d097355eb
Eli <coloredflare@gmail.com>
2015-05-19 09:34:05 +0300

    delete login test

--- index.php
+++ index.php
@@ -5,15 +5,8 @@
 	</head>
 	<body>
 		<h1>PufuList</h1>
-		<h3> Log in Test </h3>
 
-		<form action="lists.php" method="GET">
-			<label for="username">Username: </label>
-			<input id="username" type="text" name="username">
-			<input type="submit" value="Login">
-		</form>
-
-		<h3>Real Login</h3>
+		<h3>Login</h3>
 		<form action="login.php" method="POST">
 			<label for="username">Username: </label>
 			<input id="username" type="text" name="username">
