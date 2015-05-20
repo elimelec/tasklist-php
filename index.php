09eb597a126dcfcd18970cec6bc90aaa98dcdc00
Eli <coloredflare@gmail.com>
2015-05-20 23:55:41 +0300

    remove comment

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<!--		<link rel="stylesheet" type="index/css" href="css/style.css">-->
 		<link rel="stylesheet" type="text/css" href="css/theme.css">
 	</head>
 	<body>
