050cebfeb74e763630d9b09a9d6769b45a801917
MihaelaNicoleta <raumiha21@gmail.com>
2015-05-20 21:05:32 +0300

    first design with css

--- lists.php
+++ lists.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="index/css" href="css/lists_style.css" /> 
+		<title>Lists</title>
+	</head>
+
+	<body>
+	<div class="wrapper">
+
 <?php
 
 	include_once "session.php";
@@ -6,20 +17,44 @@
 	if ($is_logged) {
 		$sql_username = "SELECT user FROM users WHERE id = $user_id";
 		$username = $mysqli->query($sql_username)->fetch_object()->user;
+?>
 
-		echo "Welcome back, $username! You, awesome being. <br>";
+		<div class="header">
+			<?php echo "Welcome back, $username! You, awesome being. <br>"; ?>
 
+		</div>
+
+<?php
+	
 		$sql_select_lists = "SELECT id, name FROM lists WHERE user_id = " . $user_id;
 		$results = $mysqli->query($sql_select_lists);
+?>
 
-		echo "<a href='add_list.php?user_id=$user_id'>[add new list]</a> <br>";
+	<div class="content">
+			<div class="whatever">
+					<form action="<?php echo "add_list.php?user_id= '" . $user_id . "' "; ?>" method="POST">
+					<input class="button" type="submit" value="[add new list]">
+					</form>
+			</div>
+
+			<div class="paper"> 			
+
+<?php
 		while($row = $results->fetch_assoc()) {
 			$id = $row["id"];
 			$name = $row["name"];
-			echo "$name <a href='tasks.php?list_id=$id'>[show tasks]</a> <a href='edit_list.php?list_id=$id'>[edit]</a> <a href='delete_list.php?list_id=$id'>[delete]</a> <br>";
+			echo "<p> $name <a href='tasks.php?list_id=$id'>[show tasks]</a> <a href='edit_list.php?list_id=$id'>[edit]</a> <a href='delete_list.php?list_id=$id'>[delete]</a> </p>";
 		}
 	}
 	else {
 		header("Location: /");
 	}
 ?>
+			</div>
+	</div>
+	</div>
+	</body>
+
+
+
+</html>
--- tasks.php
+++ tasks.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="index/css" href="css/tasks_style.css" /> 
+		<title>Tasks</title>
+	</head>
+
+	<body>
+	<div class="wrapper">
+
+
+
+
 <?php
 
 	include "connect.php";
@@ -8,17 +22,37 @@
 
 		$sql_select_tasks = "SELECT id, name, checked FROM tasks WHERE list_id = " . $list_id;
 		$results = $mysqli->query($sql_select_tasks);
+?>
 
-		echo "<a href='lists.php'>[home]</a><br>";
-		echo "<a href='add_task.php?list_id=$list_id'>[add new task]</a> <br>";
+<div class="content">
+			<div class="whatever">
+					<form action="<?php echo "lists.php"; ?>" method="POST">
+					<input class="button_home" type="submit" value="[home]"></form>
+					<form action="<?php echo "add_task.php?list_id= '" . $list_id . "' "; ?>" method="POST">
+					<input class="button" type="submit" value="[add new task]"></form>
+			</div>
+
+
+			<div class="paper"> 
+
+<?php			
 		while($row = $results->fetch_assoc()) {
 			$id = $row["id"];
 			$name = $row["name"];
 			$checked = $row["checked"];
 
 			$checked_string = $checked == 0 ? "[ ]" : "[x]";
-			echo "$checked_string $name <a href='check_task.php?task_id=$id'> [check/uncheck] </a> <a href='edit_task.php?task_id=$id'>[edit]</a> <a href='delete_task.php?task_id=$id'>[delete]</a> <br>";
+			echo "<p> $checked_string $name <a href='check_task.php?task_id=$id'> [check/uncheck] </a> <a href='edit_task.php?task_id=$id'>[edit]</a> <a href='delete_task.php?task_id=$id'>[delete]</a> </p>";
 
 		}
 	}
 ?>
+
+		</div>
+
+	</div>
+	</body>
+
+
+
+</html>
