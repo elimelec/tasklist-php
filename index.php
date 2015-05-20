57c2bacc54830ff70f8918e32d53ef9b274a66bf
Eli <coloredflare@gmail.com>
2015-05-20 23:22:28 +0300

    round buttons <3

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PufuList</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!--		<link rel="stylesheet" type="index/css" href="css/style.css">-->
+		<link rel="stylesheet" type="text/css" href="css/theme.css">
 	</head>
 	<body>
-		<h1>PufuList</h1>
+		<div>
+			<header class="shadow-border--big">PufuList</header>
 
-		<h3>Login</h3>
-		<form action="login.php" method="POST">
-			<label for="username">Username: </label>
-			<input id="username" type="text" name="username">
-			<br>
-			<label for="password">Password: </label>
-			<input id="password" type="password" name="password">
-			<br>
-			<input type="submit" value="Login">
-		</form>
+			<div class="content login-page shadow-border">
+				<div class="login">
+					<h4>Already have an account?</h4>
+					<form action="login.php" method="POST">
+						<label for="username">Username: </label>
+						<input id="username" type="text" name="username">
+						<br>
+						<label for="password">Password: </label>
+						<input id="password" type="password" name="password">
+						<br>
+						<input class="shadow-border button--round button--small" type="submit" value="Login">
+					</form>
+				</div>
 
-		<h3> Sign Up </h3>
-		<form action="signup.php" method="POST">
-			<label for="username">Username: </label>
-			<input id="username" type="text" name="username"> <br>
-			<label for="password">Password: </label>
-			<input id="password" type="password" name="password"> <br>
-			<label for="check_password">Check password: </label>
-			<input id="check_password" type="password" name="check_password"> <br>
-			<input type="submit" value="Sign Up">
-		</form>
-
+				<div class="register">
+					<h4>Join today, It's free!</h4>
+					<form action="signup.php" method="POST">
+						<label for="username">Username: </label>
+						<input id="username" type="text" name="username"> <br>
+						<label for="password">Password: </label>
+						<input id="password" type="text" name="password"> <br>
+						<label for="check_password">Check password: </label>
+						<input id="check_password" type="text" name="check_password"> <br>
+						<input class="shadow-border button--round button--small" type="submit" value="Sign Up">
+					</form>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
