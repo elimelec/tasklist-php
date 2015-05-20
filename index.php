40294f1300fe5e7a74bcf69c08e3f54e032f6c3c
Eli <coloredflare@gmail.com>
2015-05-20 23:25:50 +0300

    remove labels

--- index.php
+++ index.php
@@ -13,11 +13,9 @@
 				<div class="login">
 					<h4>Already have an account?</h4>
 					<form action="login.php" method="POST">
-						<label for="username">Username: </label>
-						<input id="username" type="text" name="username">
+						<input id="username" type="text" name="username" placeholder="User">
 						<br>
-						<label for="password">Password: </label>
-						<input id="password" type="password" name="password">
+						<input id="password" type="password" name="password" placeholder="Password">
 						<br>
 						<input class="shadow-border button--round button--small" type="submit" value="Login">
 					</form>
@@ -26,12 +24,13 @@
 				<div class="register">
 					<h4>Join today, It's free!</h4>
 					<form action="signup.php" method="POST">
-						<label for="username">Username: </label>
-						<input id="username" type="text" name="username"> <br>
-						<label for="password">Password: </label>
-						<input id="password" type="text" name="password"> <br>
-						<label for="check_password">Check password: </label>
-						<input id="check_password" type="text" name="check_password"> <br>
+						<input id="username" type="text" name="username" placeholder="User">
+						<br>
+
+						<input id="password" type="text" name="password" placeholder="Password">
+						<br>
+
+						<input id="check_password" type="text" name="check_password" placeholder="Check Password"> <br>
 						<input class="shadow-border button--round button--small" type="submit" value="Sign Up">
 					</form>
 				</div>
