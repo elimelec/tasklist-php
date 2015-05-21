7f53db0c4ef3a8ee8c95427692888c27efa4e79b
Eli <coloredflare@gmail.com>
2015-05-21 08:35:57 +0300

    br on new line

--- index.php
+++ index.php
@@ -22,11 +22,10 @@ page_header();
 		<form action="signup.php" method="POST">
 			<input id="username" type="text" name="username" placeholder="User">
 			<br>
-
 			<input id="password" type="text" name="password" placeholder="Password">
 			<br>
-
-			<input id="check_password" type="text" name="check_password" placeholder="Check Password"> <br>
+			<input id="check_password" type="text" name="check_password" placeholder="Check Password">
+			<br>
 			<input class="shadow-border button--round button--small" type="submit" value="Sign Up">
 		</form>
 	</div>
