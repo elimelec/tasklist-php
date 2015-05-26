1ba27e7611932d69dcc54cdecdd97bde4cf90296
Eli <coloredflare@gmail.com>
2015-05-26 10:43:01 +0300

    remove function for now

--- page.php
+++ page.php
@@ -161,20 +161,6 @@ function page_lists_new_form() {
 	page_form($form_data);
 }
 
-function page_tasks_new_form() {
-	$form_data = array(
-		'action' => "add_list.php" ,
-		'inputs' => array(
-				get_page_form_input("text", "list_name", "List name", false) ,
-			) ,
-		'submit' => array(
-			'value' => "New" ,
-			'right' => false ,
-		) ,
-	);
-	page_form($form_data);
-}
-
 function get_page_form_text_input($name, $placeholder = "", $new_line = true) {
 	return get_page_form_input("text", $name, $placeholder, $new_line);
 }
