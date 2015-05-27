4babb13c09f301c18b5fb4f9a4a3961525a1e731
Eli <coloredflare@gmail.com>
2015-05-27 20:31:17 +0300

    re-intend + include seesion.php

--- edit_list.php
+++ edit_list.php
@@ -1,40 +1,40 @@
 <?php
 
-	include "sql.php";
+include_once "session.php";
+include_once "sql.php";
 
-	if(isset($_GET['submit'])) {
+if(isset($_GET['submit'])) {
 
-		$list_name = $_GET['list_name'];
+	$list_name = $_GET['list_name'];
+	$list_id = $_GET['list_id'];
+
+	$sql_select_list = "SELECT user FROM lists JOIN users ON users.id = user_id WHERE lists.id = $list_id";
+	$result = mysqli_query($link, $sql_select_list);
+	$result = mysqli_fetch_assoc($result);
+	$username = $result['user'];
+
+	$sql_update = "UPDATE lists SET name = '$list_name' WHERE id = $list_id";
+	mysqli_query($link, $sql_update);
+
+	header("Location: lists.php");
+}
+elseif(isset($_GET['list_id'])) {
 		$list_id = $_GET['list_id'];
 
-		$sql_select_list = "SELECT user FROM lists JOIN users ON users.id = user_id WHERE lists.id = $list_id";
-		$result = mysqli_query($link, $sql_select_list);
+		$sql_select_list_name = "SELECT name FROM lists WHERE lists.id = $list_id";
+		$result = mysqli_query($link, $sql_select_list_name);
 		$result = mysqli_fetch_assoc($result);
-		$username = $result['user'];
+		$list_name = $result['name'];
+	?>
 
-		$sql_update = "UPDATE lists SET name = '$list_name' WHERE id = $list_id";
-		mysqli_query($link, $sql_update);
+	<form method="GET">
+		<input type="hidden" name = "list_id" value="<?=$list_id?>">
+		<label for="list_name">List name: </label>
+		<input id="list_name" type="text" name="list_name" value="<?=$list_name?>">
+		<input type="submit" name="submit" value="Ok">
+	</form>
 
-		header("Location: lists.php");
-	}
-
-	elseif(isset($_GET['list_id'])) {
-			$list_id = $_GET['list_id'];
-
-			$sql_select_list_name = "SELECT name FROM lists WHERE lists.id = $list_id";
-			$result = mysqli_query($link, $sql_select_list_name);
-			$result = mysqli_fetch_assoc($result);
-			$list_name = $result['name'];
-		?>
-
-		<form method="GET">
-			<input type="hidden" name = "list_id" value="<?=$list_id?>">
-			<label for="list_name">List name: </label>
-			<input id="list_name" type="text" name="list_name" value="<?=$list_name?>">
-			<input type="submit" name="submit" value="Ok">
-		</form>
-
-		<?php
-	}
+	<?php
+}
 
 ?>
--- edit_task.php
+++ edit_task.php
@@ -1,37 +1,38 @@
 <?php
 
-	include "sql.php";
+include_once "session.php";
+include_once "sql.php";
 
-	if(isset($_GET['submit'])) {
+if(isset($_GET['submit'])) {
 
-		$task_name = $_GET['task_name'];
+	$task_name = $_GET['task_name'];
+	$task_id = $_GET['task_id'];
+
+	$task = get_task($task_id);
+	$list_id = $task['list_id'];
+
+	$task['name'] = $task_name;
+
+	update_task($task);
+
+	header("Location: tasks.php?list_id=$list_id");
+}
+
+elseif(isset($_GET['task_id'])) {
 		$task_id = $_GET['task_id'];
 
 		$task = get_task($task_id);
-		$list_id = $task['list_id'];
+		$task_name = $task['name'];
+	?>
 
-		$task['name'] = $task_name;
+	<form method="GET">
+		<input type="hidden" name = "task_id" value="<?=$task_id?>">
+		<label for="task_name">Task name: </label>
+		<input id="task_name" type="text" name="task_name" value="<?=$task_name?>">
+		<input type="submit" name="submit" value="Ok">
+	</form>
 
-		update_task($task);
-
-		header("Location: tasks.php?list_id=$list_id");
-	}
-
-	elseif(isset($_GET['task_id'])) {
-			$task_id = $_GET['task_id'];
-
-			$task = get_task($task_id);
-			$task_name = $task['name'];
-		?>
-
-		<form method="GET">
-			<input type="hidden" name = "task_id" value="<?=$task_id?>">
-			<label for="task_name">Task name: </label>
-			<input id="task_name" type="text" name="task_name" value="<?=$task_name?>">
-			<input type="submit" name="submit" value="Ok">
-		</form>
-
-		<?php
-	}
+	<?php
+}
 
 ?>
