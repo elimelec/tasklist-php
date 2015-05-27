bfa6ba16fe0505a36d14389d9a341f287b8ea8b9
Eli <coloredflare@gmail.com>
2015-05-27 21:06:27 +0300

    use the new extracted functions

--- edit_list.php
+++ edit_list.php
@@ -15,18 +15,15 @@ if(isset($_GET['submit'])) {
 	header("Location: lists.php");
 }
 elseif(isset($_GET['list_id'])) {
-		$list_id = $_GET['list_id'];
+	$list_id = $_GET['list_id'];
 
-		$sql_select_list_name = "SELECT name FROM lists WHERE lists.id = $list_id";
-		$result = mysqli_query($link, $sql_select_list_name);
-		$result = mysqli_fetch_assoc($result);
-		$list_name = $result['name'];
+	$list = get_list($list_id);
 	?>
 
 	<form method="GET">
 		<input type="hidden" name = "list_id" value="<?=$list_id?>">
 		<label for="list_name">List name: </label>
-		<input id="list_name" type="text" name="list_name" value="<?=$list_name?>">
+		<input id="list_name" type="text" name="list_name" value="<?=$list['name']?>">
 		<input type="submit" name="submit" value="Ok">
 	</form>
 
