cec262ea5257a47dc9c89b4a610e5b4c6a073fbd
Eli <coloredflare@gmail.com>
2015-05-28 23:00:53 +0300

    update schema

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 27, 2015 at 10:03 PM
+-- Generation Time: May 28, 2015 at 09:00 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.4.37
 
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `items` (
 --
 
 INSERT INTO `items` (`id`, `hash`, `name`, `type`, `parent`, `user_id`) VALUES
-(1, '18696c489cea9e22c90753ebb75aac9d', 'Food', 'group', 0, 1),
+(1, '18696c489cea9e22c90753ebb75aac9d', 'Foods', 'group', 0, 1),
 (2, '104c75265f861106b6ea5a9a58bf13b8', 'Pizza', 'task', 0, 1);
 
 -- --------------------------------------------------------
@@ -76,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `items_tasks` (
   `id` int(11) NOT NULL,
   `checked` tinyint(1) NOT NULL DEFAULT '0',
   `item_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `items_tasks`
@@ -84,30 +84,6 @@ CREATE TABLE IF NOT EXISTS `items_tasks` (
 
 INSERT INTO `items_tasks` (`id`, `checked`, `item_id`) VALUES
 (1, 0, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lists`
---
-
-DROP TABLE IF EXISTS `lists`;
-CREATE TABLE IF NOT EXISTS `lists` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `lists`
---
-
-INSERT INTO `lists` (`id`, `name`, `user_id`) VALUES
-(1, 'Fruits', 2),
-(2, 'Movies', 2),
-(3, 'Anime', 1),
-(4, 'Pizza', 1),
-(5, 'Vegetables', 2);
 
 -- --------------------------------------------------------
 
@@ -120,50 +96,15 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `id` int(11) NOT NULL,
   `token` varchar(32) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sessions`
 --
 
 INSERT INTO `sessions` (`id`, `token`, `user_id`) VALUES
-(1, 'f209e978489990ceba2535e56b25661a', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
-DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `checked` tinyint(1) NOT NULL,
-  `list_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `tasks`
---
-
-INSERT INTO `tasks` (`id`, `name`, `checked`, `list_id`) VALUES
-(1, 'Bananas', 1, 1),
-(2, 'Oranges', 0, 1),
-(3, 'Apples', 0, 1),
-(4, 'Pomegranate', 0, 1),
-(5, 'Revolver', 0, 2),
-(6, 'Hysteria', 1, 2),
-(7, 'Benjamin Buthon', 1, 2),
-(8, 'Atonement', 0, 2),
-(9, 'Dragon Ball', 0, 3),
-(10, 'One Piece', 0, 3),
-(11, 'Death Note', 1, 3),
-(12, 'Prosciutto', 0, 4),
-(13, 'Salami', 0, 4),
-(14, 'Capriciosa', 0, 4),
-(15, 'Potato', 0, 5),
-(16, 'Tomato', 1, 5);
+(2, 'f3445704ef904bc2eba27c5050cfc174', 1),
+(3, 'e2edd6993f3d99cf001e040a86a85a1d', 1);
 
 -- --------------------------------------------------------
 
@@ -209,21 +150,9 @@ ALTER TABLE `items_tasks`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `lists`
---
-ALTER TABLE `lists`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `sessions`
 --
 ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -250,22 +179,12 @@ ALTER TABLE `items_groups`
 -- AUTO_INCREMENT for table `items_tasks`
 --
 ALTER TABLE `items_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `lists`
---
-ALTER TABLE `lists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `users`
 --
