86f2e0d4305767aaf0608c48b23ff2e89b72105b
Eli <coloredflare@gmail.com>
2015-05-31 19:16:16 +0300

    reorder ifs

--- index.php
+++ index.php
@@ -93,11 +93,11 @@ $request = $_SERVER['REQUEST_URI'];
 if (preg_match('/^\/$/', $request)) {
 	$page_login();
 }
-elseif (preg_match('/^\/items$/', $request)) {
-	$page_items();
-}
 elseif (preg_match('/^\/logout$/', $request)) {
 	$page_logout();
+}
+elseif (preg_match('/^\/items$/', $request)) {
+	$page_items();
 }
 elseif (preg_match('/^\/items\/([0-9]+)$/', $request, $matches)) {
 	$page_items($matches[1]);
