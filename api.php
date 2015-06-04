23de91dea8d58911bef3b83eb06908beeb88dd30
Eli <coloredflare@gmail.com>
2015-06-04 14:03:29 +0300

    boring api function

---
+++ api.php
@@ -0,0 +1,5 @@
+<?php
+
+function api($request) {
+	echo $request;
+}
--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 include_once "session.php";
 include_once "page.php";
 include_once "controller.php";
+include_once "api.php";
 
-controller_print_page($_SERVER['REQUEST_URI']);
+
+$request = $_SERVER['REQUEST_URI'];
+if (preg_match('/^\/api/', $request)) {
+	$request = substr($request, 4);
+	api($request);
+}
+else {
+	controller_print_page($request);
+}
