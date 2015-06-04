ffe8139458b4ff074175049a354cc58452072af8
Eli <coloredflare@gmail.com>
2015-06-04 14:38:35 +0300

    login api

--- api.php
+++ api.php
@@ -1,8 +1,22 @@
 <?php
+include_once "sql.php";
 
 function api($request) {
-	if (preg_match('/^\/items\/([0-9]+)$/', $request, $matches)) {
+	if (preg_match('/^\/login\/([a-z]+)\/([a-z]+$)/', $request, $matches)) {
+		login($matches[1], $matches[2]);
+	}
+	elseif (preg_match('/^\/items\/([0-9]+)$/', $request, $matches)) {
 		api_items($matches[1]);
+	}
+}
+
+function login($username, $password) {
+	$hash = md5($username . $password . time());
+	$user = get_user($username, $password);
+	if ($user) {
+		$user_id = $user['id'];
+		set_session($hash, $user_id);
+		echo json_encode($hash);
 	}
 }
 
