a372f18c61fa51f7205fd5e1a7a69ee4185e16c9
Eli <coloredflare@gmail.com>
2015-06-04 14:15:44 +0300

    print items

--- api.php
+++ api.php
@@ -1,5 +1,13 @@
 <?php
 
 function api($request) {
-	echo $request;
+	if (preg_match('/^\/items\/([0-9]+)$/', $request, $matches)) {
+		api_items($matches[1]);
+	}
+}
+
+function api_items($parent) {
+	// FIXME: don't hardcode user id!
+	$items = get_items(1, $parent);
+	echo json_encode($items);
 }
